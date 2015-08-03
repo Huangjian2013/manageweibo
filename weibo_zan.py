cd281cd9d7ac6692da7b117218aa7dc60b828fd4
guohuangjian <guohuangjian@xiaomi.com>
2015-08-03 23:10:05 +0800

    Weibo like function

--- weibo_zan.py
+++ weibo_zan.py
@@ -2,6 +2,7 @@
 #coding=utf8
 
 try:
+	import time
 	import urllib
 	import urllib2
 except ImportError:
@@ -20,24 +21,28 @@ def get_mid():
 	return mid
 
 def like_mid(mid):
+
 	url = 'http://www.weibo.com/aj/v6/like/add?ajwvr=6'
 	data = {'version':'mini', 'qid':'heart', 'loc':'profile','location':'page_100505_home'}
 	data['mid'] = mid
 	data = urllib.urlencode(data)
+
 	print data
-	http_headers = {'User-Agent':'Mozilla/5.0 (X11; Linux i686; rv:8.0) Gecko/20100101 Firefox/8.0'}
-	req  = urllib2.urlopen(
+
+	http_headers = {'User-Agent':'Mozilla/5.0 (X11; Linux i686; rv:8.0) Gecko/20100101 Firefox/8.0', 'Referer':'http://weibo.com/u/5648162302', 'X-Requested-With':'XMLHttpRequest', 'Origin':'http://www.weibo.com', 'Content-Type':'application/x-www-form-urlencoded', 'Accept-Language':'zh-CN,zh;q=0.8', 'Accept':'*/*', 'Accept-Encoding':'gzip, deflate'}
+	
+
+	req  = urllib2.Request(
 		url = url,
 	        data = data,
-#        	headers = http_headers
+        	headers = http_headers
 	)
 
-#	httpHandler = urllib2.HTTPHandler(debuglevel=1)
-#	httpsHandler = urllib2.HTTPSHandler(debuglevel=1)
 
-#	opener = urllib2.build_opener(httpHandler, httpsHandler)
+#	opener = urllib2.build_opener(req, httpHandler, httpsHandler)
 #	response = opener.open(req, data)
-	return req.read()
-	
+#	req = urllib2.urlopen(url, httpHandler, httpsHandler)
+	result = urllib2.urlopen(req)
+	return result.read()
 	
 
